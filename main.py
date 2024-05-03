1b66ad21ff09fdbd94cc249d5327da1a09c88c36
Saurabh Sharam <saurabh.nayla@gmail.com>
2024-05-03 23:46:30 +0530

    eroor catching

--- main.py
+++ main.py
@@ -19,31 +19,33 @@ def player_move(icon):
 
     print("Your turn player {}".format(number))
 
-    choice = int(input("Enter your move(1-9): ").strip())
-    if board[choice-1] ==" ":
-        board[choice-1] = icon
-    else:
-        print()
-        print("That space is take!")
+    while True:
+        try:
+            choice = int(input("Enter your move(1-9): ").strip())
+            if choice < 1 or choice > 9:
+                print("Please enter a number between 1 and 9.")
+                continue
+            if board[choice-1] == " ":
+                board[choice-1] = icon
+                break
+            else:
+                print("That space is taken!")
+        except ValueError:
+            print("Invalid input! Please enter a number.")
 
 def is_victory(icon):
-    if (board[0] == icon and board[1] ==  icon and board[2] == icon) or \
-         (board[3] == icon and board[4] ==  icon and board[5] == icon) or \
-         (board[6] == icon and board[7] ==  icon and board[8] == icon) or \
-         (board[0] == icon and board[3] ==  icon and board[6] == icon) or \
-         (board[1] == icon and board[4] ==  icon and board[7] == icon) or \
-         (board[2] == icon and board[5] ==  icon and board[8] == icon) or \
-         (board[0] == icon and board[4] ==  icon and board[8] == icon) or \
-         (board[2] == icon and board[4] ==  icon and board[6]== icon):
-        return True
-    else:
-        return False
+    win_conditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Columns
+        [0, 4, 8], [2, 4, 6]              # Diagonals
+    ]
+    for condition in win_conditions:
+        if all(board[i] == icon for i in condition):
+            return True
+    return False
 
 def is_draw():
-    if" " not in board:
-        return True
-    else:
-        return False
+    return " " not in board
 
 while True:
     print_board()
@@ -53,13 +55,13 @@ while True:
         print("X Wins! Congratulations!")
         break
     elif is_draw():
-        print("its a draw!")
+        print("It's a draw!")
         break
     player_move("O")
+    print_board()
     if is_victory("O"):
-        print_board()
         print("O Wins! Congratulations!")
         break
     elif is_draw():
-        print("its a draw!")
+        print("It's a draw!")
         break

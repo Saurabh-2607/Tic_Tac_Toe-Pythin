ff6d1123757ea31f9aa8300e0c92befc57f2e532
Saurabh Sharam <saurabh.nayla@gmail.com>
2024-05-03 23:48:50 +0530

    added want to play again

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-board = [" " for i in range(9)]
-
-def print_board():
+def print_board(board):
     row1 = "|{}|{}|{}|".format(board[0],board[1],board[2])
     row2 = "|{}|{}|{}|".format(board[3],board[4],board[5])
     row3 = "|{}|{}|{}|".format(board[6],board[7],board[8])
@@ -11,7 +9,7 @@ def print_board():
     print(row3)
     print()
 
-def player_move(icon):
+def player_move(icon, board):
     if icon == "X":
         number = 1
     elif icon == "O":
@@ -33,7 +31,7 @@ def player_move(icon):
         except ValueError:
             print("Invalid input! Please enter a number.")
 
-def is_victory(icon):
+def is_victory(icon, board):
     win_conditions = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Rows
         [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Columns
@@ -44,24 +42,53 @@ def is_victory(icon):
             return True
     return False
 
-def is_draw():
+def is_draw(board):
     return " " not in board
 
-while True:
-    print_board()
-    player_move("X")
-    print_board()
-    if is_victory("X"):
+def print_result(winner, board):
+    print_board(board)
+    if winner == "X":
         print("X Wins! Congratulations!")
-        break
-    elif is_draw():
-        print("It's a draw!")
-        break
-    player_move("O")
-    print_board()
-    if is_victory("O"):
+    elif winner == "O":
         print("O Wins! Congratulations!")
-        break
-    elif is_draw():
+    else:
         print("It's a draw!")
-        break
+
+def reset_board():
+    return [" " for i in range(9)]
+
+def play_game():
+    board = reset_board()
+    winner = None
+    while True:
+        print_board(board)
+        player_move("X", board)
+        if is_victory("X", board):
+            winner = "X"
+            break
+        elif is_draw(board):
+            break
+        print_board(board)
+        player_move("O", board)
+        if is_victory("O", board):
+            winner = "O"
+            break
+    print_result(winner, board)
+
+def ask_play_again():
+    while True:
+        play_again = input("Do you want to play again? (yes/no): ").strip().lower()
+        if play_again in ['yes', 'no']:
+            return play_again == 'yes'
+        else:
+            print("Invalid input! Please enter 'yes' or 'no'.")
+
+def main():
+    while True:
+        play_game()
+        if not ask_play_again():
+            print("Goodbye!")
+            break
+
+if __name__ == "__main__":
+    main()
